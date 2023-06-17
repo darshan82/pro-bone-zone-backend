20935375c070ee769f1ffacbc9b08045b0f4575e
Darshan Lal <darshanmandhwani12@gmail.com>
2023-06-17 19:35:33 +0500

    code

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 const app = require("./app")
 const config = require("./config");
 
-
 const database = require('./sqlconnect');
-const https = require('https');
-const fs = require('fs');
 
-const options = {
-  key: fs.readFileSync('private.key'),
-  cert: fs.readFileSync('certificate.crt')
-};
 
-const server = https.createServer(options, app);
-
-server.listen(config.PORT, (err,res) =>
+app.listen(config.PORT, (err,res) =>
 {
   console.log('server start');
 });

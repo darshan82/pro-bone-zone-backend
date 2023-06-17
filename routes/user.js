b2d2e0a37e72654f6a3700dee18ed1f49a7e092e
Darshan Lal <darshanmandhwani12@gmail.com>
2023-06-17 12:38:20 +0500

    code

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const catchAsync = require("../utils/catchAsync");
 const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/user")
-const { validationResult, check } = require('express-validator');
+const { validationResult, check, body } = require('express-validator');
 
 router.post('/login',
     [
@@ -12,14 +12,13 @@ router.post('/login',
 
 router.post('/signup',
     [
-        check('permit').notEmpty().withMessage('Permit is required').isIn(['admin', 'licensee', 'staff']).withMessage('Invalid permit value'),
-        check('nameFirst').notEmpty().withMessage('First name is required'),
-        check('nameLast').notEmpty().withMessage('Last name is required'),
-        check('phone').notEmpty().withMessage('Phone number is required'),
-        check('email').isEmail().withMessage('Invalid email'),
-        check('pass').notEmpty().withMessage('Password is required'),
-        check('territoryId').notEmpty().withMessage('territory-id is required'),
-        check('licenseeId').notEmpty().withMessage('licensee-id is required'),
+        body('permit').notEmpty().withMessage('Permit is required').isIn(['admin', 'licensee', 'staff']).withMessage('Invalid permit value'),
+        body('nameFirst').notEmpty().withMessage('First name is required'),
+        body('nameLast').notEmpty().withMessage('Last name is required'),
+        body('phone').notEmpty().withMessage('Phone number is required'),
+        body('email').isEmail().withMessage('Invalid email'),
+        body('pass').notEmpty().withMessage('Password is required'),
+        body('territoryId').notEmpty().withMessage('territory-id is required'),
     ]
     , controller.signup);
 

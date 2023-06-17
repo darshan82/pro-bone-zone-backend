adafedaca882c23625b883b424e3c68a251b5769
Darshan Lal <darshanmandhwani12@gmail.com>
2023-06-17 18:20:40 +0500

    ss

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 const app = require("./app")
 const config = require("./config");
-// const https = require('https');
-// const fs = require('fs');
+
 
 const database = require('./sqlconnect');
+const https = require('https');
+const fs = require('fs');
 
+const options = {
+  key: fs.readFileSync('private.key'),
+  cert: fs.readFileSync('certificate.crt')
+};
 
-// const options = {
-//   key: fs.readFileSync('private.key'),
-//   cert: fs.readFileSync('certificate.crt')
-// };
+const server = https.createServer(options, app);
 
-// const server = https.createServer(options, app);
-
-app.listen(config.PORT, (err,res) =>
+server.listen(config.PORT, (err,res) =>
 {
   console.log('server start');
 });

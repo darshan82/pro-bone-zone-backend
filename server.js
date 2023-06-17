0d5e45b580a73000737ed44fcb285a58c5ad1884
Darshan Lal <darshanmandhwani12@gmail.com>
2023-06-17 16:17:28 +0500

    install certificate locally

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const app = require("./app")
 const config = require("./config");
+const https = require('https');
+const fs = require('fs');
 
 const database = require('./sqlconnect');
 
 
-app.listen(config.PORT, (err,res) =>
+const options = {
+  key: fs.readFileSync('private.key'),
+  cert: fs.readFileSync('certificate.crt')
+};
+
+const server = https.createServer(options, app);
+
+server.listen(config.PORT, (err,res) =>
 {
   console.log('server start');
 });

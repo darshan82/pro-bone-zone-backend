ac71d4f635db16b342a947e53897bb1bd1c1986f
Darshan Lal <darshanmandhwani12@gmail.com>
2023-06-23 23:20:00 +0500

    asdasd

--- controllers/territory.js
+++ controllers/territory.js
@@ -2,23 +2,27 @@ const catchAsync = require("../utils/catchAsync")
 const database = require('../sqlconnect');
 const { validationResult } = require("express-validator");
 
-exports.territory = catchAsync(async (req, res, next) =>
-{
+exports.territory = catchAsync(async (req, res, next) => {
     const connection = database.getConnection();
-
-    connection.query('SELECT * FROM territory', (error, results) =>
-    {
-        if (error)
-        {
-            console.error('Error executing query:', error);
-            res.status(500).json({ error: 'Error executing query:', error });
-            return;
-        }
-
-        // Process the query results
-        res.json(results);
+  
+    const query = `
+      SELECT territory.*, user.*
+      FROM territory
+      INNER JOIN user ON territory.\`licensee-id\` = user.id
+    `;
+  
+    connection.query(query, (error, results) => {
+      if (error) {
+        console.error('Error executing query:', error);
+        res.status(500).json({ error: 'Error executing query:', error });
+        return;
+      }
+  
+      // Process the query results
+      res.json(results);
     });
-})
+  });
+  
 
 
 exports.addTerritory = catchAsync(async (req, res, next) =>

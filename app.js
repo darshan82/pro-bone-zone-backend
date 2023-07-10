c4b3667c69fa8e9f694f977867145145e90f470f
Darshan Lal <darshanmandhwani12@gmail.com>
2023-07-10 23:50:40 +0500

    disable backend

--- app.js
+++ app.js
@@ -19,46 +19,46 @@ const blogs = require("./routes/blogs.js");
 const { verifyTheToken } = require('./middlewares/Auth');
 
 // Serve the 'uploads' directory as a static folder
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+// app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 
-app.use("/appointment", appointment);
-app.use("/customer", verifyTheToken, customer);
-app.use("/event", verifyTheToken, event);
-app.use("/promotion", promotion);
+// app.use("/appointment", appointment);
+// app.use("/customer", verifyTheToken, customer);
+// app.use("/event", verifyTheToken, event);
+// app.use("/promotion", promotion);
 
-app.use("/staff", verifyTheToken, staff);
-app.use("/sponsor", verifyTheToken, sponsor);
-app.use("/resource", verifyTheToken, resource);
-app.use("/user", user);
-app.use("/territory", verifyTheToken, territory);
-app.use("/blogs", verifyTheToken, blogs);
+// app.use("/staff", verifyTheToken, staff);
+// app.use("/sponsor", verifyTheToken, sponsor);
+// app.use("/resource", verifyTheToken, resource);
+// app.use("/user", user);
+// app.use("/territory", verifyTheToken, territory);
+// app.use("/blogs", verifyTheToken, blogs);
 
-app.get("/global/states", (req, res) =>
-{
-    const connection = database.getConnection();
+// app.get("/global/states", (req, res) =>
+// {
+//     const connection = database.getConnection();
 
-    connection.query('SELECT * FROM states', (error, results) =>
-    {
-        if (error)
-        {
-            console.error('Error executing query:', error);
-            res.status(500).json({ error: 'Error executing query:', error });
-            return;
-        }
+//     connection.query('SELECT * FROM states', (error, results) =>
+//     {
+//         if (error)
+//         {
+//             console.error('Error executing query:', error);
+//             res.status(500).json({ error: 'Error executing query:', error });
+//             return;
+//         }
 
-        // Process the query results
-        res.json(results);
-    });
-});
+//         // Process the query results
+//         res.json(results);
+//     });
+// });
 
-app.get('/', (req, res) =>
-{
-    res.send('Hello, World!');
-});
+// app.get('/', (req, res) =>
+// {
+//     res.send('Hello, World!');
+// });
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb)
@@ -75,39 +75,39 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 // Route to handle file/image upload
-app.post('/upload', upload.single('file'), (req, res) =>
-{
-    // Access the uploaded file details
-    const uploadedFile = req.file;
+// app.post('/upload', upload.single('file'), (req, res) =>
+// {
+//     // Access the uploaded file details
+//     const uploadedFile = req.file;
 
-    if (!uploadedFile)
-    {
-        // No file was uploaded
-        res.status(400).json({ error: 'No file uploaded' });
-        return;
-    }
+//     if (!uploadedFile)
+//     {
+//         // No file was uploaded
+//         res.status(400).json({ error: 'No file uploaded' });
+//         return;
+//     }
 
-    // File/image was successfully uploaded
-    const filePath = path.join('uploads', uploadedFile.filename);
-    res.json({
-        message: 'File uploaded successfully',
-        file: {
-            filename: uploadedFile.originalname,
-            size: uploadedFile.size,
-            mimetype: uploadedFile.mimetype,
-            path: filePath, // File path on your server
-        },
-    });
-});
+//     // File/image was successfully uploaded
+//     const filePath = path.join('uploads', uploadedFile.filename);
+//     res.json({
+//         message: 'File uploaded successfully',
+//         file: {
+//             filename: uploadedFile.originalname,
+//             size: uploadedFile.size,
+//             mimetype: uploadedFile.mimetype,
+//             path: filePath, // File path on your server
+//         },
+//     });
+// });
 
 // 404 Error handling middleware
-app.use((req, res, next) =>
-{
-    res.status(404).json({
-        error: true,
-        message: `Cannot find ${req.originalUrl} on this server`,
-    });
-});
+// app.use((req, res, next) =>
+// {
+//     res.status(404).json({
+//         error: true,
+//         message: `Cannot find ${req.originalUrl} on this server`,
+//     });
+// });
 
 // Error handling middleware
 app.use((err, req, res, next) =>
